d12bb260dfdf5e4d1a4eafe420fcbc9e0fa4bb1d
Avinash Verma <avinashverma.it@gmail.com>
2022-05-28 12:46:31 +0530

    Header component

---
+++ src/components/header/Header.test.tsx
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react'
+
+import { Header } from './Header'
+
+describe('Header Component', () => {
+  it('renders', () => {
+    const { container } = render(<Header>Application Name</Header>)
+    expect(container).toMatchSnapshot()
+  })
+
+})
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { Flex } from 'shared-components'
+
+interface IHeaderProps {
+  children?: React.ReactNode
+}
+
+export const Header: React.FC<IHeaderProps> = ({ children }) => {
+  return (
+    <Flex
+      css={{
+        height: '$6',
+        backgroundColor: '$tonal500',
+        alignItems: 'center',
+        px: '$5'
+      }}
+    >
+      {children}
+    </Flex>
+  )
+}
