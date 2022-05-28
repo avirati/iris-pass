1591c49048e7b6be5c332666734ff32551943a37
Avinash Verma <avinashverma.it@gmail.com>
2022-05-28 12:53:57 +0530

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Flex } from '@atom-learning/components'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import { Text } from 'shared-components'
+import { Header } from 'components/header';
+import { Dashboard } from 'views/dashboard'
 
 export const App: React.FC = () => {
   return (
-    <Flex>
-      Hello
-    </Flex>
+    <BrowserRouter>
+      <Header>
+        <Text size="xl" css={{ color: 'white' }}>Password Manager</Text>
+      </Header>
+      <Routes>
+        <Route path="/" element={<Dashboard />}/>
+        <Route path="/dashboard" element={<Dashboard />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
