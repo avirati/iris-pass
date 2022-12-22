3f97cfa19f6f14d9f5bc34f1f68d5853d18a9f42
Avinash Verma <avinashverma.it@gmail.com>
2022-12-22 09:09:36 +0530

    Dark bg - wip

--- src/views/dashboard/Dashboard.tsx
+++ src/views/dashboard/Dashboard.tsx
@@ -1,21 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-import { ContentContainer } from 'components/content-container'
-import { usePasswords } from 'hooks/use-passwords'
-import { PasswordCard } from 'components/password-card'
+import { ContentContainer } from 'components/content-container';
+import { usePasswords } from 'hooks/use-passwords';
+import { PasswordCard } from 'components/password-card';
 
-import { EmptyPasswordScreen } from './EmptyPasswordScreen'
+import { EmptyPasswordScreen } from './EmptyPasswordScreen';
 
 export const Dashboard: React.FC = () => {
-  const { passwords } = usePasswords()
+  const { passwords } = usePasswords();
 
   return (
-    <ContentContainer css={{ gap: '$4', height: 'calc(100vh - 128px)' }}>
-      {
-        passwords.length > 0
-          ? passwords.map((password) => <PasswordCard password={password} key={password.id}/>)
-          : <EmptyPasswordScreen />
-      }
+    <ContentContainer
+      css={{ gap: '$4', height: 'calc(100vh - 128px)', bg: '$tonal500' }}
+    >
+      {passwords.length > 0 ? (
+        passwords.map((password) => (
+          <PasswordCard password={password} key={password.id} />
+        ))
+      ) : (
+        <EmptyPasswordScreen />
+      )}
     </ContentContainer>
-  )
-}
+  );
+};
--- src/views/dashboard/EmptyPasswordScreen.tsx
+++ src/views/dashboard/EmptyPasswordScreen.tsx
@@ -5,15 +5,17 @@ import { EmptyState, Icon } from 'shared-components';
 
 export const EmptyPasswordScreen: React.FC = () => {
   return (
-    <EmptyState size='sm'>
+    <EmptyState size='sm' css={{ my: 'auto' }}>
       <EmptyState.Image
         as={Icon}
         is={Dashboard}
         size='xl'
-        css={{ color: '$tonal200', my: 'auto' }}
+        css={{ color: '$tonal200' }}
       />
-      <EmptyState.Title>No passwords found!</EmptyState.Title>
-      <EmptyState.Body>
+      <EmptyState.Title css={{ color: '$tonal200' }}>
+        No passwords found!
+      </EmptyState.Title>
+      <EmptyState.Body css={{ color: '$tonal200' }}>
         Generate most secure passwords in a click! And sync them across devices
         :)
       </EmptyState.Body>
