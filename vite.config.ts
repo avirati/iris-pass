0f2faa995d17f106b9d77362c9cc345f963f2d34
Avinash Verma <avinashverma.it@gmail.com>
2022-12-23 20:21:34 +0530

    Fix Web dev build

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 8080,
+  },
+  root: path.join(__dirname, './src/renderer'),
+});
