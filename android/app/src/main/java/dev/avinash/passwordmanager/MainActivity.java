651006a1229ad12470a3b993a411874ca51197c7
Avinash Verma <avinashverma.it@gmail.com>
2022-12-23 20:53:47 +0530

    Integrate capacitor

---
+++ android/app/src/main/java/dev/avinash/passwordmanager/MainActivity.java
@@ -0,0 +1,5 @@
+package dev.avinash.passwordmanager;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.avinash.passwordmanager',
+  appName: 'Password Manager',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
